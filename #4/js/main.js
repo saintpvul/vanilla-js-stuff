0df5f0a104800234d47751deee3fb26379e4ae02
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-07 02:58:04 +0600

    feat: complete sort by years ex.

--- #4/js/main.js
+++ #4/js/main.js
@@ -83,3 +83,13 @@ const totalYears = inventors.reduce((total, inventor) => {
 }, 0);
 
 console.log(totalYears);
+
+// 5. sort inventors by years lived
+
+const oldest = inventors.sort(function (a, b) {
+  const lastGuy = a.passed - a.year;
+  const nextGuy = b.passed - b.year;
+  return lastGuy > nextGuy ? -1 : 1;
+});
+
+console.table(oldest);
