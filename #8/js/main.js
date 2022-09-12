250ba21a81c3e033b7440a994595f1f4d4756f7a
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-12 18:37:57 +0600

    feat: complete day#8 challenge

--- #8/js/main.js
+++ #8/js/main.js
@@ -1,17 +1,50 @@
-const panels = document.querySelectorAll(".panel");
+const canvas = document.querySelector("#draw");
 
-function toggleOpen() {
-  this.classList.toggle("open");
-}
+const ctx = canvas.getContext("2d");
 
-function toggleActive(e) {
-  if (e.propertyName.includes("flex")) {
-    this.classList.toggle("open-active");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.strokeStyle = "#BADA55";
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
+ctx.lineWidth = 50;
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+let direction = true;
+
+function draw(e) {
+  if (!isDrawing) return;
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+  hue++;
+  if (hue >= 360) {
+    hue = 0;
+  }
+  if (ctx.lineWidth >= 60 || ctx.lineWidth <= 10) {
+    direction = !direction;
+  }
+  if (direction) {
+    ctx.lineWidth++;
+  } else {
+    ctx.lineWidth--;
   }
 }
 
-panels.forEach((panel) => panel.addEventListener("click", toggleOpen));
+canvas.addEventListener("mousedown", (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
 
-panels.forEach((panel) =>
-  panel.addEventListener("transitionend", toggleActive)
-);
+canvas.addEventListener("mousemove", draw);
+
+canvas.addEventListener("mouseup", () => (isDrawing = false));
+
+canvas.addEventListener("mouseout", () => (isDrawing = false));
