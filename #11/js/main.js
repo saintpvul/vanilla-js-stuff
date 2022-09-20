e5fea1ee8dbf8ff791e83fe562492c47a4192ef2
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-21 00:59:48 +0600

    feat: add new features

--- #11/js/main.js
+++ #11/js/main.js
@@ -11,4 +11,41 @@ const ranges = player.querySelectorAll(".player__slider");
 
 //build out functions
 
+function togglePlay() {
+  const method = video.paused ? "play" : "pause";
+  video[method]();
+}
+
+function updateButton() {
+  const icon = this.paused ? "►" : "❚ ❚";
+  toggle.textContent = icon;
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
 //hook up the event listeners
+
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", handleProgress);
+
+toggle.addEventListener("click", togglePlay);
+
+skipButtons.forEach((button) => button.addEventListener("click", skip));
+
+ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
+
+ranges.forEach((range) =>
+  range.addEventListener("mousemove", handleRangeUpdate)
+);
