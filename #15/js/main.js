3d0ccdb7e3101b9202e9a0b034d1efd5d5376e46
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-01-30 19:53:40 +0600

    fix: delete comma from items list. set limit of tapas

--- #15/js/main.js
+++ #15/js/main.js
@@ -4,27 +4,35 @@ const addItems = document.querySelector(".add-items"),
 
 function addItem(e) {
   e.preventDefault();
-
   const text = this.querySelector("[name=item]").value;
-
   const item = {
     text,
     done: false,
   };
 
-  items.push(item);
+  if (items.length < 7) {
+    items.push(item);
+  } else {
+    alert("There is too much tapas, don't you think so?");
+  }
   populateList(items, itemsList);
+  localStorage.setItem("items", JSON.stringify(items));
   this.reset();
 }
 
 function populateList(plates = [], platesList) {
-  platesList.innerHTML = plates.map((plate, i) => {
-    return `<li><label for="">${plate.text}</label></li>`;
-  });
+  platesList.innerHTML = plates
+    .map(
+      (plate, i) => `
+			<li>
+				<input type="checkbox" data-index=${i} id="item${i}" ${
+        plate.done ? "checked" : ""
+      } />
+				<label for="item${i}">${plate.text}</label>
+			</li>
+		`
+    )
+    .join("");
 }
 
 addItems.addEventListener("submit", addItem);
-
-// !!! TODO
-// need to find dot when add new item
-// limit items list with 5 items
