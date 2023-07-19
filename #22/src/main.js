ed1f2fcf92761725fadb1498232c4d9b6287573d
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-20 01:36:02 +0600

    feat: add two blocks(about and locations) along with their logic, source files and animations

--- #22/src/main.js
+++ #22/src/main.js
@@ -1,73 +1,323 @@
-// need to change all elements of the webpage, create diff blocks with content, add sliders and forms, some animation / activity
+// All animation elements
+let headerAnimationTimeout;
+let bgImageInterval;
+let aboutShowcaseLeftInterval;
+let aboutShowcaseRightInterval;
 
-//heading animation
+// Webpage timers handler
 
-//setting the bg image and change it every 2mins
-//cant get access to this files from js
-const header = document.querySelector("header");
+function stopAllTimers() {
+    clearTimeout(headerAnimationTimeout);
+    clearInterval(bgImageInterval);
+    clearInterval(aboutShowcaseLeftInterval);
+    clearInterval(aboutShowcaseRightInterval);
+}
+
+// Function to start all timers
+function startAllTimers() {
+    animateHeader();
+    startBgImageInterval();
+    startAboutShowcaseIntervals();
+}
+
+// Heading animation
+
+// Heading text animation
+const headerHeading = document.getElementById("header-head");
+headerHeading.style.opacity = 0;
+headerHeading.style.transition = "opacity 1s";
+
+const headerHeadingText = ["Embrace the Chaos", "Shop", "MessDepot"];
+
+// Function to animate header text when page is opened or reloaded
+function animateHeader() {
+    setTimeout(() => {
+        headerHeading.textContent = headerHeadingText[0];
+        headerHeading.style.opacity = 1;
+
+        setTimeout(() => {
+            headerHeading.style.opacity = 1;
+
+            setTimeout(() => {
+                headerHeading.style.opacity = 0;
+
+                setTimeout(() => {
+                    headerHeading.textContent = headerHeadingText[1];
+                    headerHeading.style.opacity = 1;
+
+                    setTimeout(() => {
+                        headerHeading.style.opacity = 1;
+
+                        setTimeout(() => {
+                            headerHeading.style.opacity = 0;
+
+                            setTimeout(() => {
+                                headerHeading.textContent =
+                                    headerHeadingText[2];
+                                headerHeading.style.opacity = 1;
+                            }, 1000);
+                        }, 1000);
+                    }, 500);
+                }, 1000);
+            }, 500);
+        }, 1000);
+    }, 1000);
+}
+// Setting the bg image and change it every 2mins
+
+const header = document.getElementById("heading-bg");
 const images = [
-    "./img/op-1.jpg",
-    "./img/op-2.jpg",
-    "./img/op-3.jpg",
-    "./img/op-4.jpg",
-    "./img/op-5.jpg",
-    "./img/op-6.jpg",
+    "./src/img/bg/op-1.jpg",
+    "./src/img/bg/op-2.jpg",
+    "./src/img/bg/op-4.jpg",
+    "./src/img/bg/op-5.jpg",
+    "./src/img/bg/op-6.jpg",
 ];
 
-let currentImageCounter = 2;
+let currentImageCounter = 1;
 
-function setCurrentBackgroundImage() {
-    header.style.backgroundImage = `url(${images[currentImageCounter]})`;
-    currentImageCounter++;
-    if (currentImageCounter >= images.length) {
+// Function to update bg img of chosen element
+
+function setImageByUrl(img) {
+    return `url(${img})`;
+}
+
+function setImageWithLinearGradientBySide(img, side) {
+    return `linear-gradient(
+            to ${side},
+            rgba(255, 255, 255, 0) 70%,
+            rgba(245, 245, 245, 1) 100%
+        ),
+        ${setImageByUrl(img)}`;
+}
+
+function setNewImage(element, imageScenario) {
+    // Image transition animation, so that the user does not see the image loading process
+    element.style.transition = "opacity 1s";
+    element.style.opacity = "0";
+    setTimeout(() => {
+        element.style.backgroundImage = imageScenario;
+        element.style.opacity = "1";
+    }, 1000);
+}
+
+function getNexBgImage() {
+    if (currentImageCounter >= 5) {
         currentImageCounter = 0;
+    }
+    let currentImage = images[currentImageCounter];
+    currentImageCounter++;
+    return currentImage;
+}
+
+// Eave last bg img
+function saveBackgroundImage(imageUrl) {
+    localStorage.setItem("lastImage", imageUrl);
+}
+
+function getBackgroundImage() {
+    return localStorage.getItem("lastImage");
+}
+
+// Set last bg image if it exists
+let lastImg = getBackgroundImage();
+if (lastImg) {
+    setNewImage(header, setImageByUrl(lastImg));
+}
+
+// Function to start the interval to set background image every 2 mins
+function startBgImageInterval() {
+    bgImageInterval = setInterval(() => {
+        const nextImage = getNexBgImage();
+        saveBackgroundImage(nextImage);
+        setNewImage(header, setImageByUrl(nextImage));
+    }, 2 * 60 * 1000);
+}
+
+// Nav logo-button logic
+
+function reloadPage() {
+    showAbout();
+    window.location.reload();
+}
+
+// Main section
+
+// Getting access
+const about = document.querySelector(".about");
+const aboutBtn = document.querySelector(".about-btn");
+const aboutShowcase = document.querySelector(".about-showcase");
+const leftImg = document.querySelector(".about-left");
+const rightImg = document.querySelector(".about-right");
+
+const catalog = document.querySelector(".catalog");
+const catalogBtn = document.querySelector(".catalog-btn");
+
+const catalogLeft = document.querySelector(".catalog-left");
+const catalogRight = document.querySelector(".catalog-right");
+
+const locations = document.querySelector(".locations");
+const locationsBtn = document.querySelector(".locations-btn");
+const locationsShowcase = document.querySelector(".locations-showcase");
+const locationsLeft = document.querySelector(".locations-left");
+const locationsRight = document.querySelector(".locations-right");
+// Navbar buttons functions
+
+const transitionDuration = 1000; // 1 second
+const shortTransitionDuration = 300; // 0.3 second
+
+function hideElement(element) {
+    element.style.opacity = "0";
+    setTimeout(() => {
+        element.style.display = "none";
+    }, transitionDuration);
+}
+
+function showElement(element) {
+    element.style.display = "block";
+    setTimeout(() => {
+        element.style.opacity = "1";
+    }, 0);
+}
+
+function showAbout() {
+    catalogBtn.style.color = "white";
+    locationsBtn.style.color = "white";
+    aboutBtn.style.color = "grey";
+    if (about.style.display === "block") {
+        return;
+    } else {
+        hideElement(locations);
+        hideElement(locationsShowcase);
+        hideElement(catalog);
+        showElement(about);
+        showElement(aboutShowcase);
     }
 }
 
-setInterval(() => {
-    setCurrentBackgroundImage();
-}, 2 * 60 * 10);
-
-// heading text animation
-const mainHeading = document.getElementById("main-head");
-mainHeading.style.opacity = 0;
-mainHeading.style.transition = "opacity 1s";
-
-const mainHeadingText = ["Embrace the Chaos", "Shop", "MessDepot"];
-
-setTimeout(() => {
-    mainHeading.textContent = mainHeadingText[0];
-    mainHeading.style.opacity = 1;
-
-    setTimeout(() => {
-        mainHeading.style.opacity = 1;
-
-        setTimeout(() => {
-            mainHeading.style.opacity = 0;
-
-            setTimeout(() => {
-                mainHeading.textContent = mainHeadingText[1];
-                mainHeading.style.opacity = 1;
-
-                setTimeout(() => {
-                    mainHeading.style.opacity = 1;
-
-                    setTimeout(() => {
-                        mainHeading.style.opacity = 0;
-
-                        setTimeout(() => {
-                            mainHeading.textContent = mainHeadingText[2];
-                            mainHeading.style.opacity = 1;
-                        }, 1000);
-                    }, 1000);
-                }, 500);
-            }, 1000);
-        }, 500);
-    }, 1000);
-}, 1000);
-
-// main section
-
-function reloadPage() {
-    window.location.reload();
+function showCatalog() {
+    aboutBtn.style.color = "white";
+    locationsBtn.style.color = "white";
+    catalogBtn.style.color = "grey";
+    if (catalog.style.display === "block") {
+        return;
+    } else {
+        hideElement(locations);
+        hideElement(aboutShowcase);
+        hideElement(about);
+        hideElement(aboutShowcase);
+        showElement(catalog);
+    }
 }
+
+function showLocations() {
+    aboutBtn.style.color = "white";
+    catalogBtn.style.color = "white";
+    locationsBtn.style.color = "grey";
+    if (locations.style.display === "flex") {
+        return;
+    } else {
+        hideElement(about);
+        hideElement(aboutShowcase);
+        hideElement(catalog);
+        showElement(locations);
+        showElement(locationsShowcase);
+    }
+}
+
+// About-showcase images and it's update
+
+const aboutShowcaseImages = [
+    "./src/img/about/clothes1.jpg",
+    "./src/img/about/clothes2.jpg",
+    "./src/img/about/clothes3.jpg",
+    "./src/img/about/clothes4.jpg",
+    "./src/img/about/clothes5.jpg",
+    "./src/img/about/collection.jpg",
+    "./src/img/about/fabric-knitting.jpg",
+    "./src/img/about/sewing-process.jpg",
+    "./src/img/about/style-development.jpg",
+    "./src/img/about/workshop.jpg",
+];
+
+let leftImageCount = 0;
+let rightImageCount = 1;
+
+const getAboutShowcaseImages = (side) => {
+    const n = aboutShowcaseImages.length;
+    let currentImage;
+    if (side === "left") {
+        if (leftImageCount >= n) {
+            leftImageCount = 0;
+        }
+        currentImage = aboutShowcaseImages[leftImageCount];
+        leftImageCount += 2;
+    } else if (side === "right") {
+        if (rightImageCount >= n) {
+            rightImageCount = 1;
+        }
+        currentImage = aboutShowcaseImages[rightImageCount];
+        rightImageCount += 2;
+    }
+    return currentImage;
+};
+// Function to animate about showcase side-bg images
+function startAboutShowcaseIntervals() {
+    aboutShowcaseLeftInterval = setInterval(() => {
+        const nextLeftImage = getAboutShowcaseImages("left");
+        setNewImage(
+            leftImg,
+            setImageWithLinearGradientBySide(nextLeftImage, "right")
+        );
+    }, 15 * 1000);
+
+    aboutShowcaseRightInterval = setInterval(() => {
+        const nextRightImage = getAboutShowcaseImages("right");
+        setNewImage(
+            rightImg,
+            setImageWithLinearGradientBySide(nextRightImage, "left")
+        );
+    }, 25 * 1000);
+}
+
+startAllTimers();
+
+// add locations map
+function initMap() {
+    const map = L.map("map");
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    // Add markers for New York City and London
+    L.marker([40.7128, -74.006]).addTo(map).bindPopup("New York City");
+    L.marker([51.5074, -0.1278]).addTo(map).bindPopup("London");
+
+    // Create a bounds object that includes both locations
+    const bounds = L.latLngBounds([
+        [40.7128, -74.006],
+        [51.5074, -0.1278],
+    ]);
+    console.log(bounds);
+
+    // Set the map view to fit the bounds
+    map.fitBounds(bounds);
+
+    // Add eventListener to location blocks
+    const newYorkBlock = document.getElementById("new-york");
+    const londonBlock = document.getElementById("london");
+
+    // Center the map on the mouseovered location
+    newYorkBlock.addEventListener("mouseover", function () {
+        map.setView([40.7338, -74.056], 13);
+    });
+
+    londonBlock.addEventListener("mouseover", function () {
+        map.setView([51.5244, -0.1748], 13);
+    });
+}
+
+// Initialize the map
+initMap();
